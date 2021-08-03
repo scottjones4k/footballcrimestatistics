1b58c24935f724c65cce6f369e7dcbe7bc2085e2
Scott Jones <scott.jones@optix.co.uk>
2021-08-03 08:46:21 +0100

    Project structure

---
+++ FootballCrimeStatistics.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FootballCrimeStatistics.Logic
+{
+    public class Class1
+    {
+    }
+}
---
+++ FootballCrimeStatistics.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace FootballCrimeStatistics.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
